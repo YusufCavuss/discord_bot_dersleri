df1e90c00543716b5dee2f17e01623bc16651ced
Yusuf Çavuşoğlu <78086967+YusufCavuss@users.noreply.github.com>
2021-04-20 17:30:04 +0300

    bot.js

---
+++ bölüm_1/bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} adlı botun ile giriş yaptım!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.channel.send('Aleyküm Selam');
+  }
+  if (msg.content.toLowerCase() === 'yusuf') {
+    msg.channel.send('En kral adam!');
+  }
+  if (msg.content.toLowerCase() === 'adam') {
+    msg.channel.send('Yusuf!');
+  }
+});
+
+client.login('ODM0MDIwMDIxODYwMDQwNzE1.YH6zrA.0kuHQeHQNxsJqkkjYnS7FIIR4AI');
